c7072d3627ec267185609b31dbc07d86d348400f
Jgohnert <goh22001@byui.edu>
2024-01-11 14:20:42 -0700

    Completed the guess number activity

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomNumber = new Random();
+        int number = randomNumber.Next(1, 1000);
+
+        int userGuess = 0;
+
+        while (userGuess != number)
+        {
+            Console.Write("Guess the number: ");
+            string chosenNumber = Console.ReadLine();
+
+            int x = int.Parse(chosenNumber);
+
+            if (x > number)
+            {
+                Console.WriteLine("Guess lower");
+            }
+            else if (x < number)
+            {
+                Console.WriteLine("Guess Higher");
+            }
+            else
+            {
+                Console.WriteLine($"Correct! The correct number is {number}");
+            }
+        }
+
     }
 }
