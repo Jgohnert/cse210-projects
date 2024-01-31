58d5c9eda33a9f0c832afa6a37983b44515f6653
Jgohnert <goh22001@byui.edu>
2024-01-30 22:33:28 -0700

    Split the values on a csv file

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -5,18 +5,15 @@ public class Journal
     public List<Entry> _entryList = new List<Entry>();
 
     public void SaveFile(string fileName, List<Entry> entriesToSave)
-    {
+{
     using (StreamWriter outputFile = new StreamWriter(fileName))
     {
         foreach (Entry entry in entriesToSave)
         {
-            outputFile.WriteLine(
-$@"Prompt: {entry._onePrompt}
-Date: {entry._date}
-Entry: {entry._userEntry}");
+            outputFile.WriteLine($"{entry._date},{entry._onePrompt},{entry._userEntry}");
         }
     }
-    }
+}
 
     public List<Entry> LoadFromFile(string fileName)
     {
@@ -25,23 +22,21 @@ Entry: {entry._userEntry}");
 
         string[] lines = System.IO.File.ReadAllLines(fileName);
 
-        for (int i = 0; i < lines.Length; i += 3)
-    {
-        if (i + 2 < lines.Length)
+        for (int i = 0; i < lines.Length; i++)
         {
-            Entry entry = new Entry
-            {
-                _onePrompt = lines[i + 0].Substring(8),
-                _date = lines[i + 1].Substring(5),
-                _userEntry = lines[i + 2].Substring(7)
-            };
+            string[] values = lines[i].Split(',');
 
-            savedJournal.Add(entry);
+            if (values.Length == 3)
+            {
+                Entry entry = new Entry
+                {
+                    _date = values[0],
+                    _onePrompt = values[1],
+                    _userEntry = values[2]
+                };
+                savedJournal.Add(entry);
+            }
         }
-    }
-    
         return savedJournal;
     }
-
-
 }
