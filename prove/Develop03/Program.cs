ec0e3a13106097b3328192f3cabcace763c1cb13
Jgohnert <goh22001@byui.edu>
2024-02-08 17:15:56 -0700

    Added the scripture to the main class.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture scripture = new Scripture("John 3:16", "For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life.");
     }
 }
