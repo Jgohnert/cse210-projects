b75131a3f81b2bfb45c8dbefd1dc1eb85d9330b6
Jgohnert <goh22001@byui.edu>
2024-02-08 17:06:23 -0700

    Finished setting up the attributes and behaviors

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Word
+{
+    private string _text;
+    private bool _hiddenWord;
+
+    // public IsHidden()
+    // {
+        
+    // }
+
+    // public GetText()
+    // {
+
+    // }
+
+    // public Hide()
+    // {
+
+    // }
+
+    // public Show()
+    // {
+
+    // }
+
+    public Word(string text, bool hidden)
+    {
+
+    }
+}
