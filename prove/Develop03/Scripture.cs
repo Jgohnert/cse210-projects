1504c9cf7ab9daa19b3f08a148c104efd1f417fa
Jgohnert <goh22001@byui.edu>
2024-02-08 17:15:08 -0700

    finished adding the behaviors and attributes

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Scripture
+{
+    private List<string> _wordList;
+    private Reference _scriptureReference;
+
+    public void HideWords()
+    {
+
+    }
+
+    // public bool IsAllHidden()
+    // {
+
+    // }
+
+    // public string GetText()
+    // {
+
+    // }
+
+    public Scripture(string reference, string text)
+    {
+
+    }
+
+    Scripture(string reference, List<Word> words)
+    {
+        
+    }
+}
